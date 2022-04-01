24ac9c5d0cdde7bdb63771b20764176cdf92777a
Vlad Kravchenko <57967177+VladKravchenko05082000@users.noreply.github.com>
2022-04-01 16:14:41 +0300

    Lesson 6

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState(
+    'Open up App.js to start working on your app!'
+  );
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{text}</Text>
+      <Button title="Press on me" onPress={() => setText('Text is changed')} />
     </View>
   );
 }
